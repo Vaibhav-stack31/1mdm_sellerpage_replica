bc65bdaec80fb608979bb4603647522ca691453f
Vaibhav Tiwari <tvaibhav23it@student.mes.ac.in>
2025-06-21 15:52:41 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   preview: {
+    host: true, // same as '0.0.0.0'
     port: 4444,
-    host: true, // Allows access via 0.0.0.0 (external network)
+    allowedHosts: ['www.vaibhav.code4bharat.com']
   }
 })
