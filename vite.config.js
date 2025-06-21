f2b63b856ef47dc39324589e22bedc67197f9a2a
Vaibhav Tiwari <tvaibhav23it@student.mes.ac.in>
2025-06-21 15:48:23 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    port: 4444,
+    host: true, // Allows access via 0.0.0.0 (external network)
+  }
 })
